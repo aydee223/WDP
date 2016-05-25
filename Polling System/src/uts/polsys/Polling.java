39fed8054cba963020a4f019936f3cbf6773cf9a
rodrigur <rosa.rodriguez@transport.nsw.gov.au>
2016-05-25 23:28:35 +1000

    Update Polling.java

--- Polling System/src/uts/polsys/Polling.java
+++ Polling System/src/uts/polsys/Polling.java
@@ -4,6 +4,9 @@ import java.util.*;
 import java.io.Serializable;
 import javax.xml.bind.annotation.*;
 
+//commented by rosa
+//import uts.wsd.User;
+
 @XmlRootElement(name = "polling")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Polling implements Serializable{
@@ -16,4 +19,24 @@ public class Polling implements Serializable{
 	public ArrayList<User> getUsers(){
 		return users;
 	}
+
+	//added by rosa
+    public User login(String username, String password) {
+        // For each user in the list...
+        for (User user : users) {
+        	System.out.println("Users user is " + user);
+            if (user.getUserName().equals(username) && user.getPassword().equals(password))
+                return user; // Login correct. Return this user.
+        }
+        return null; // Login incorrect. Return null.
+    }
+/*
+    public void addUser(User user) {
+        users.add(user);
+    }
+    
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+ */
 }
